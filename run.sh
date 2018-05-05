c659c8db37d347fb069da2458ef2c9b08efa1079
Ych <huyuncongh@gmail.com>
2018-05-05 16:26:52 -0700

    new submodule

---
+++ run.sh
@@ -0,0 +1,2 @@
+sudo docker run -v /home/ych/Research/2018Spring/CS294-144/BCSM_SGX/rust-sgx-sdk:/root/sgx -v /home/ych/Research/2018Spring/CS294-144/BCSM_SGX/core:/root/sgx/samplecode/BCSM_SGX -ti --device /dev/isgx baiduxlab/sgx-rust
+#sudo docker run -v /home/ych/Research/2018Spring/baiduSDK/rust-sgx-sdk:/root/sgx -ti --device /dev/isgx baiduxlab/sgx-rust
